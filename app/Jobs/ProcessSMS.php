220d83b8d3afda82d1fa6750d30e0ad05a3cd5ed
Lee Mbuuga <mbugua.dev@gmail.com>
2019-09-25 01:53:35 +0300

    fix typo in import

--- app/Jobs/ProcessSMS.php
+++ app/Jobs/ProcessSMS.php
@@ -2,7 +2,7 @@
 
 namespace App\Jobs;
 use App\Models\Outbox;
-use App\Request\ATClient;
+use App\Requests\ATClient;
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
