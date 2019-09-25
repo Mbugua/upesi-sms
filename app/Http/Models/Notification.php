18e493b2b1abc3a0f46bb1e408d197375100083c
Lee Mbuuga <mbugua.dev@gmail.com>
2019-09-25 14:19:16 +0300

    set unique col for notification messegeID

--- app/Http/Models/Notification.php
+++ app/Http/Models/Notification.php
@@ -1,12 +1,13 @@
 <?php
 
 namespace App\Models;
-
-use Illuminate\Database\Eloquent\Model;
-
+GoldSpecDigital\LaravelEloquentUUID\Database\Eloquent\Model;
+// use Illuminate\Database\Eloquent\Model;
+use Ramsey\Uuid\Uuid;
 class Notification extends Model
 {
     protected $table='notification';
+    protected $uuidVersion=1;
     //{"phoneNumber":"+254797561830","failureReason":"DeliveryFailure","retryCount":"0","id":"ATXid_47f18d7e4952b41692e1e3a4e686f28d","status":"Failed","networkCode":"63902"}
     protected $fillable=[
         'outbox_reference',
@@ -19,4 +20,14 @@ class Notification extends Model
         'network'
 
     ];
+
+        /**
+     * @throws \Exception
+     * @return string
+     */
+    protected function generateUuid(): string
+    {
+        // UUIDv3 has been used here, but you can also use UUIDv5.
+        return Uuid::uuid3(Uuid::NAMESPACE_DNS, 'upesi-sms')->toString();
+    }
 }
