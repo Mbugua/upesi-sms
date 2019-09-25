24e3fb0ac38eb041053a78afa65b182bbe90cad8
Lee Mbuuga <mbugua.dev@gmail.com>
2019-09-25 17:25:07 +0300

    Inbox models fix import

--- app/Jobs/ProcessInbox.php
+++ app/Jobs/ProcessInbox.php
@@ -1,13 +1,13 @@
 <?php
 
 namespace App\Jobs;
-
+use App\Models\Inbox;
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
-use App\Models\Inbox;
+
 class ProcessInbox implements ShouldQueue
 {
     use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
