81a7e9767837b4e98e283796e6190db3dd4a5c25
Lee Mbuuga <mbugua.dev@gmail.com>
2019-09-25 16:10:21 +0300

    remove long indexes

--- app/Jobs/ProcessOutbox.php
+++ app/Jobs/ProcessOutbox.php
@@ -41,7 +41,7 @@ class ProcessOutbox implements ShouldQueue
         // if($outboxResponse['status']==='success'){
         //     $outbox->cost=$outboxResponse['Recipients'];
         // }
-        var_dump($outboxResponse);
+        var_dump($outboxResponse['data']);
         Log::debug('outboxResponse >>'.\json_encode($outboxResponse));
         //commit transaction
         // $outbox->save();
