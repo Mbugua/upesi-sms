9a94c3aab9be100e0f148f437dc7489ac0c8a73c
Lee Mbugua <mbugua.dev@gmail.com>
2022-01-05 13:15:02 +0300

    Update Notification.php

--- app/Http/Models/Notification.php
+++ app/Http/Models/Notification.php
@@ -7,7 +7,7 @@ class Notification extends Model
 {
     protected $table='notification';
 
-    //{"phoneNumber":"+254797561830","failureReason":"DeliveryFailure","retryCount":"0","id":"ATXid_47f18d7e4952b41692e1e3a4e686f28d","status":"Failed","networkCode":"63902"}
+    //{"phoneNumber":"+254722000000","failureReason":"DeliveryFailure","retryCount":"0","id":"ATXid_47f18d7e4952b41692e1e3a4e686f28d","status":"Failed","networkCode":"63902"}
     protected $fillable=[
         'phoneNumber',
         'failureReason',
